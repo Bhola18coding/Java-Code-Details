e57ad5ab8ebb53b25e6441aca62a03fb32424af8
Bhola18coding <patranirmal243@gmail.com>
2026-02-11 19:20:50 +0530

    terminal

---
+++ src/Day_6_STREAM_AND_OPTIONALS/D/Terminal/a_TerminalOperationsDemo.java
@@ -0,0 +1,16 @@
+package Day_6_STREAM_AND_OPTIONALS.D.Terminal;
+
+import java.util.List;
+
+public class a_TerminalOperationsDemo {
+   public static void main(String[] args) {
+
+       /* TERMINAL OPERATOR */
+       // Terminal Operators , intermediate operator ke baad use hota hain. There are types of Terminal Operators*/
+
+       //(i) reduce()
+       // From the result we have in streams, we will make it one final result
+
+
+    }
+}
